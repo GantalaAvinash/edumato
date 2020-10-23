94f3403590c0a30d8c0769717c0f6cfb3351e7df
GantalaAvinash <gantalaavinash@gmail.com>
2020-10-23 12:34:45 +0530

    Edumato Live

---
+++ build/precache-manifest.52dd5098c5e28952e9a478804f5c81b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725f97fa3dc90fa0fa97bb15e4cc537e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "740465e78d42f83467bf",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "4c831ac58b9227ba1579",
+    "url": "/static/css/main.4d74ad94.chunk.css"
+  },
+  {
+    "revision": "740465e78d42f83467bf",
+    "url": "/static/js/2.6017e6dc.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.6017e6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c831ac58b9227ba1579",
+    "url": "/static/js/main.90e71cff.chunk.js"
+  },
+  {
+    "revision": "ff6ea3e4c70149509929",
+    "url": "/static/js/runtime-main.19fdbaa2.js"
+  }
+]);
---
+++ build/precache-manifest.ce12e936281c3ab3c1bbd781c07bde60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae63156c60e54b1bfd7fb1367f88b4a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "740465e78d42f83467bf",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "7a21c41084c703ebaac7",
+    "url": "/static/css/main.f1838b7e.chunk.css"
+  },
+  {
+    "revision": "740465e78d42f83467bf",
+    "url": "/static/js/2.6017e6dc.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.6017e6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a21c41084c703ebaac7",
+    "url": "/static/js/main.0e0d7c3e.chunk.js"
+  },
+  {
+    "revision": "ff6ea3e4c70149509929",
+    "url": "/static/js/runtime-main.19fdbaa2.js"
+  }
+]);
